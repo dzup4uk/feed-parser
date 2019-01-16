7ec46098d1830ffedbd867576b6e09ffa2f51942
dzup4uk <vitya_d@ukr.net>
2019-01-16 13:23:43 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -20,15 +20,19 @@ print('<html>', "\n", file=news_file)
 print('<head>', "\n", file=news_file)
 print('<title> Parsed feed from BBC </title>',"\n", file=news_file)
 print('</head>', "\n", file=news_file)
-print('<body>', "\n", file=news_file)
-
+print('<body onload="loadDoc()">', "\n", file=news_file)
 print("<div>", result.feed['title'], "</div>\n", file=news_file)
+print('<div id="demo"></div>\n', file=news_file)
 
 for i in result.entries:
     print("<p><em>", i['published'], "</em>&nbsp; &nbsp; <strong>", i['title'], ' </strong> <span style="text-decoration: underline;">', i['link'],  "</span></p>\n", file=news_file)
     print("<blockquote>", "\n", file=news_file)
     print(i['summary'], "\n", file=news_file)
     print("</p></blockquote>", "\n", file=news_file)
+
+js_text = "<script src=”get_version.js”></script>"
+
+print(js_text, "\n", file=news_file)
 print('</body>', "\n", file=news_file)
 print('</html>', "\n", file=news_file)
 
