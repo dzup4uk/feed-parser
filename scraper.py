f56ba5dae6334f73dc36a424f3b1f799e4378a51
dzup4uk <vitya_d@ukr.net>
2019-01-23 00:10:59 +0200

    Few updates per pylon complains

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
-import feedparser as fp
 import json
+import feedparser as fp
+
 
 
 
@@ -9,7 +10,7 @@ data['newspapers'] = {}
 
 with open('NewsPapers.json') as data_file:
     companies = json.load(data_file)
-news_file = open('index.html',"w")
+news_file = open('index.html', "w")
 
 #for key in companies:
 #    print(key)
@@ -18,7 +19,7 @@ news_file = open('index.html',"w")
 result = fp.parse(companies['bbc']['rss'])
 print('<html>', "\n", file=news_file)
 print('<head>', "\n", file=news_file)
-print('<title> Parsed feed from BBC </title>',"\n", file=news_file)
+print('<title> Parsed feed from BBC </title>', "\n", file=news_file)
 print('</head>', "\n", file=news_file)
 print('<body onload="loadDoc()">', "\n", file=news_file)
 print("<div>", result.feed['title'], "</div>\n", file=news_file)
